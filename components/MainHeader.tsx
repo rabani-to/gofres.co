9a3cbfd3b831bfba59033476ba4e81975790ff64
D3Portillo <d3portillo@gmail.com>
2023-08-04 16:54:07 +0200

    chores: rename wording + better logo resizing

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -3,10 +3,12 @@ import Logo from "./Logo"
 function MainHeader() {
   return (
     <main className="flex flex-col gap-2 mt-12 items-center justify-center">
-      <figure className="w-full max-w-[18rem] nx-text-slate-900 dark:nx-text-slate-100">
+      <figure className="w-full px-12 md:px-4 max-w-[16rem] nx-text-slate-900 dark:nx-text-slate-100">
         <Logo />
       </figure>
-      <p className="text-sm">A wagmi component libray for Ethereum</p>
+      <p className="text-sm text-center">
+        A React component libray for Ethereum
+      </p>
     </main>
   )
 }
