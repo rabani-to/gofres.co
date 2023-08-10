fb50ab228d825211915aa759e781c77ff5351dbe
D3Portillo <d3portillo@gmail.com>
2023-08-10 21:09:58 +0200

    docs: better content + add package metadata

--- components/fresco/Button/Button.tsx
+++ components/fresco/Button/Button.tsx
@@ -2,7 +2,6 @@ function Button(props: React.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
-      onClick={() => alert("Yay")}
       className={`px-4 py-2 border rounded-lg ${props.className}`}
     >
       {props.children}
